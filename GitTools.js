6ccce7f9ef2a7ab89cc4192f4ff168c7ec00e53e
石龙 <shilong@FuckWorld.local>
2016-03-17 15:43:18 +0800

    add codes

---
+++ GitTools.js
@@ -0,0 +1,21 @@
+/**
+ * Created by shilong on 16/3/17.
+ */
+
+function gitPull(branchName){
+
+}
+
+function gitPush(branchName){
+
+}
+
+function gitMerge(mergeName)
+{
+
+}
+
+
+
+
+module.exports.gitPull = gitPull;
---
+++ main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by shilong on 16/3/17.
+ */
+//#! /usr/bin/env node
+
